6b91eba40353b3c8676440b0ae4a5e52b3552377
TohamaLimo233 <yl1009xy@outlook.com>
2026-04-04 20:28:35 +0800

    喵

--- test_performance.py
+++
@@ -1,6 +0,0 @@
-import requests
-import Lib.APIs as APIs
-
-api = APIs.WeatherAPI()
-#print(api.GetLocation())
-print(api.GetWeather(latitude=30.5233, longitude=120.4727, weather_time_day=-2, weather_time_param="weather_code", current=False))
